4dfbde5a0cae715d382818b0180bbf31059a8f9f
Jesse Alas <jessealas94@gmail.com>
2019-06-25 07:50:24 -0700

    Removed old decoder code.

--- include/logic/function/decoder.h
+++ include/logic/function/decoder.h
@@ -3,15 +3,9 @@
 #include <logic/bit_count.h>
 #include <logic/bits.h>
 
-namespace logic {
-    namespace function {
-        namespace detail {
-            constexpr bit_count_t calculate_output_bits(bit_count_t input_bits) noexcept { return 1u << input_bits; }
-        }
 
-        template <bit_count_t InputN>
-        constexpr bits<detail::calculate_output_bits(InputN)> decoder(bits<InputN> input_bits) noexcept {
-            return 1u << input_bits.value();
-        }
-    }
+namespace logic::function {
+
+
+
 }
--- test/test_decoder.cpp
+++ test/test_decoder.cpp
@@ -6,9 +6,4 @@
 
 
 TEST_CASE("", "[logic][function][decoder]") {
-    constexpr auto func = &logic::function::decoder<1u>;
-    static constexpr auto decoder_1_to_2 = logic::compile<logic::default_storage>(func);
-
-    REQUIRE(decoder_1_to_2[0b0] == 1);
-    REQUIRE(decoder_1_to_2[0b1] == 2);
 }
